a3c63651bc3f10015e76c9c78cb2a645ea65f667
Sahith Aitha <85609115+aithasahith02@users.noreply.github.com>
2025-03-02 04:40:31 -0500

    On boarded file to retrieve images from the API

---
+++ data_acquisition.py
@@ -0,0 +1,36 @@
+import os
+import urllib.request
+import pandas as pd
+
+API_KEY = "AIzaSyDB5a_qba5Y7e7fR8QvZGU6Em7l-tY6LPo"
+SAVE_PATH = "C:\\Users\\sevmy\\OneDrive\\Documents\\RevolutionUC\\Vision AI Learning\\Archive"
+
+if __name__ == "__main__":
+    busStopsDF = pd.read_csv("data.txt") #get bus stop data
+
+    #api call components
+    base = "https://maps.googleapis.com/maps/api/streetview"
+    size = "size=640x640"
+    fov = "fov=120"
+    returnErrorCode = "return_error_code=true"
+    source = "source=outdoor"
+    key = f"key={API_KEY}"
+
+    nRows = busStopsDF.shape[0]
+    for i in range(nRows):
+        #location extraction
+        lat = busStopsDF.iloc[i]["stop_lat"]
+        long = busStopsDF.iloc[i]["stop_lon"]
+        stopId = busStopsDF.iloc[i]["stop_code"]
+        location = f"location={lat},{long}"
+
+        for degree in {0, 120, 240}:
+            heading = f"heading={degree}"
+            request_url = f"{base}?{size}&{location}&{fov}&{heading}&{source}&{key}&returnErrorCode" #api call construction
+            fileName = f"{stopId}-{degree}.jpeg"
+
+            urllib.request.urlretrieve(request_url, os.path.join(SAVE_PATH, fileName))
+            print("Stop:", i, "\t", fileName, "written!")
+
+
+
